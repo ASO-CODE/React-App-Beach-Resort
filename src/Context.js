7e60d6f59b0af33aa16b8a9941fa1e1bfbfc99d9
ASO-CODE <60587094+ASO-CODE@users.noreply.github.com>
2022-07-07 02:57:45 +0100

    New Commit

--- src/Context.js
+++ src/Context.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import items from "./data";
 const RoomContext = React.createContext();
 class RoomProvider extends Component {
   state = {
@@ -7,9 +8,36 @@ class RoomProvider extends Component {
     featuredRooms: [],
     loading: true,
   };
+  // getData
+
+  componentDidMount() {
+    let rooms = this.formatData(items);
+    console.log(rooms);
+    let featuredRooms = rooms.filter((room) => room.featured === true);
+    this.setState({
+      rooms,
+      featuredRooms,
+      sortedRooms: rooms,
+      loading: false,
+    });
+  }
+
+  formatData(items) {
+    let tempItems = items.map((item) => {
+      let id = item.sys.id;
+      let images = item.fields.images.map((image) => image.fields.file.url);
+      let room = { ...item.fields, images, id };
+      return room;
+    });
+    return tempItems;
+  }
+  getRoom = (cool) => {
+    let tempRooms = [...this.state.rooms];
+    const room = tempRooms.find((room) => room.cool === cool);
+  };
   render() {
     return (
-      <RoomContext.Provider value={{ ...this.state }}>
+      <RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom }}>
         {this.props.children}
       </RoomContext.Provider>
     );
